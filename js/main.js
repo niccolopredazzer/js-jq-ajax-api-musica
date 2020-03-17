da8fd291cf5edb8a8deb3fae20538aff9f54053b
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-03-17 23:12:22 +0100

    Inserito selettore

--- js/main.js
+++ js/main.js
@@ -2,29 +2,46 @@
 //Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
 //Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
 
-    var source = $("#card-template").html();
-    var template = Handlebars.compile(source);
-    $.ajax({
-        url: 'https://flynn.boolean.careers/exercises/api/array/music',
-        method: 'GET',
-        success: function (data) {
-            var arrayAlbums = data.response;
-            for (var i = 0; i < arrayAlbums.length; i++) {
-                var album = arrayAlbums[i];
-                var oggettoAlbum = {
-                    immagine: album.poster,
-                    titolo: album.title,
-                    autore: album.author,
-                    anno: album.year,
-                    genere: album.genre,
-                }
-                var cardDaAppendere = template(oggettoAlbum);
-                $('.card-container').append(cardDaAppendere);
-            }
+var source = $("#card-template").html();
+var template = Handlebars.compile(source);
 
-        },
-        error: function () {
-            alert('errore');
+$('.selettore-genere').change(function(){
+    var selezione = $(this).val();
+    if (selezione == '') {
+        $('.card').show();
+    } else {
+        $('.card').each(function () {
+            if (selezione.toLowerCase() == $(this).data('genere').toLowerCase()) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            };
+        });
+    }
+});
+
+
+$.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/array/music',
+    method: 'GET',
+    success: function (data) {
+        var arrayAlbums = data.response;
+        for (var i = 0; i < arrayAlbums.length; i++) {
+            var album = arrayAlbums[i];
+            var oggettoAlbum = {
+                immagine: album.poster,
+                titolo: album.title,
+                autore: album.author,
+                anno: album.year,
+                genere: album.genre,
+            }
+            var cardDaAppendere = template(oggettoAlbum);
+            $('.card-container').append(cardDaAppendere);
         }
 
-    })
+    },
+    error: function () {
+        alert('errore');
+    }
+
+})
